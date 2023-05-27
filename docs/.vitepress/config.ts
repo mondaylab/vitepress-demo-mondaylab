50a432440726f114788ec9a6dd9185df028ed316
Jacqueline712 <60708406+Jacqueline712@users.noreply.github.com>
2023-05-27 10:59:17 +0800

    add:增加相关配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,12 +5,49 @@ import { sidebar, nav } from './relaConf';
 export default defineConfig({
   title: 'mondaylab-demo-blog',
   description: 'A VitePress Site',
+
+  // 配置markdown写作风格
+  // markdown: {
+  //   toc: {
+  //     level: [1, 2, 3, 4]
+  //   },
+  //   headers: {
+  //     level: [1, 2, 3, 4]
+  //   },
+  //   // https://github.com/valeriangalliat/markdown-it-anchor#usage
+  //   anchor: {
+  //     // permalink: anchor.permalink.headerLink()
+  //   },
+  //   lineNumbers: true // 让代码块中实现行号
+
+  //   // config: (md) => {
+  //   //   md.use(demoBlockPlugin, {
+  //   //     cssPreprocessor: 'less'
+  //   //   });
+  //   // }
+  // },
+
   themeConfig: {
     logo: '/avatar.png',
     // https://vitepress.dev/reference/default-theme-config
     nav: nav,
-
     sidebar: sidebar,
+
+    search: {
+      provider: 'local'
+    },
+
+    outline: {
+      level: [2, 6],
+      label: '目录'
+    },
+
+    i18nRouting: true,
+
+    carbonAds: {
+      code: 'your-carbon-code',
+      placement: 'your-carbon-placement'
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
