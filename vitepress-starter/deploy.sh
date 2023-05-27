7508233cb8c6dbe093e8bf372d049e260a9bc66e
Jacqueline712 <60708406+Jacqueline712@users.noreply.github.com>
2023-05-27 11:19:45 +0800

    add:部署

--- vitepress-starter/deploy.sh
+++ vitepress-starter/deploy.sh
@@ -13,7 +13,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Jacqueline712/ vitepress-demo-mondaylab.git master:gh-pages
+# Jacqueline712/mondaylab-blog 替换为自己的用户名和对应的仓库名
+# 意思为将master构建后的代码合并到gh-pages分支上，然后在gh-pages分支上部署~
+git push -f git@github.com:Jacqueline712/vitepress-demo-mondaylab.git master:gh-pages
 
 cd -
